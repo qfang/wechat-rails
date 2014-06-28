925e8b78ee30a0c6eac76ced8a5545bf03d60469
startupmania <irisjacky99@gmail.com>
2014-06-28 17:28:13 +1000

    fix the syntax

--- lib/wechat/responder.rb
+++ lib/wechat/responder.rb
@@ -12,12 +12,12 @@ module Wechat
 
       attr_accessor :wechat, :token
 
-      def on message_type, &block
+      def on message_type, with=nil, respond=nil, &block
         raise "Unknow message type" unless message_type.in? [:text, :image, :voice, :video, :location, :link, :event, :fallback]
         config=respond.nil? ? {} : {:respond=>respond}
         config.merge!(:proc=>block) if block_given?
 
-        if (with.present? && !message_type.in?([:text, :event]))
+        if (with != nil && !message_type.in?([:text, :event]))
           raise "Only text and event message can take :with parameters"
         else
           config.merge!(:with=>with) if with.present?
